f0c063337bb5f0dcf2e51a2cd50b849bf59a44f7
Lucas Duarte <luckasdu686@gmail.com>
2024-08-10 21:39:50 -0300

    iniciando tela demanda

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 import math
 import pandas as pd
 from openpyxl import load_workbook
-from tkinter import Tk, Button, Label, filedialog
+from tkinter import Tk, Button, Label, filedialog, messagebox
 from PIL import Image, ImageTk
 import tkinter as tk
-from tkinter import messagebox
 
+import pandas as pd
 
-# gerar executavel => pyinstaller --onefile --windowed --icon=icon.ico app.py
-# COLOCAR OS ARQUIVOS NO MESMO DIRETORIO
+# Comentários de instrução
+# Comando para gerar o executável: 
+# pyinstaller --onefile --windowed --icon=icon.ico app.py
+# Certifique-se de colocar todos os arquivos necessários no mesmo diretório
 
-# Função para arredondar o valor para o múltiplo mais próximo de multiplo
+# ---------- Funções de Cálculo e Manipulação de Dados ---------- #
+
 def marred(valor, multiplo):
+    """Arredonda o valor para o múltiplo mais próximo de multiplo."""
     return round(valor / multiplo) * multiplo
 
-# Função para calcular a quantidade a ser enviada com base na lógica especificada
 def calcular_quantidade_envio(produtos):
-
+    """Calcula a quantidade a ser enviada com base na lógica especificada."""
     resultados = []
-    for _, produto in produtos.iterrows():
 
+    for _, produto in produtos.iterrows():
         SALDO_CD = produto["SALDO_CD"]
         VENDAS = produto["VENDAS"]
         EXPOSICAO = produto["EXPOSICAO"]
@@ -31,16 +34,11 @@ def calcular_quantidade_envio(produtos):
             valor_positivo = abs(valor_calculado)
             valor_arredondado = marred(valor_positivo, UNITIZACAO)
 
-            if valor_arredondado >= SALDO_CD:
-                quantidade_a_enviar = SALDO_CD
-            else:
-                quantidade_a_enviar = valor_arredondado
-
+            quantidade_a_enviar = min(valor_arredondado, SALDO_CD)
         except Exception as e:
             print(f"Erro: {e}")
             quantidade_a_enviar = 0
 
-        # Adicionar resultados à lista
         resultados.append({
             "COD": produto['COD'],
             "DESCRICAO": produto['DESCRICAO'],
@@ -49,62 +47,96 @@ def calcular_quantidade_envio(produtos):
 
     return resultados
 
-# Função para escrever os resultados em uma nova aba do Excel
 def escrever_resultados_no_excel(nome_arquivo, resultados):
-    # Converter os resultados para um DataFrame
+    """Escreve os resultados em uma nova aba do Excel chamada 'Demanda'."""
     df_resultados = pd.DataFrame(resultados)
-
-    # Carregar a planilha existente
     book = load_workbook(nome_arquivo)
 
-    # Escrever os resultados em uma nova aba chamada 'Demanda'
     with pd.ExcelWriter(nome_arquivo, engine='openpyxl', mode='a') as writer:
         df_resultados.to_excel(writer, sheet_name='Demanda', index=False)
 
     print(f"Os resultados foram adicionados na aba 'Demanda' da planilha '{nome_arquivo}'")
 
-# Função para carregar o arquivo Excel e processar os dados
+# ---------- Funções de Interface Gráfica ---------- #
+
 def carregar_e_processar_arquivo():
+    """Carrega o arquivo Excel, processa os dados e escreve os resultados."""
     nome_arquivo = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
     if nome_arquivo:
         df_produtos = pd.read_excel(nome_arquivo)
         resultados = calcular_quantidade_envio(df_produtos)
         escrever_resultados_no_excel(nome_arquivo, resultados)
-        messagebox.showinfo("SUCESSO!","DEMANDA GERADA COM SUCESSO!")
+        messagebox.showinfo("SUCESSO!", "DEMANDA GERADA COM SUCESSO!")
 
-# Função para redimensionar a imagem
 def redimensionar_imagem(caminho_imagem, largura, altura):
+    """Redimensiona a imagem para as dimensões especificadas."""
     img = Image.open(caminho_imagem)
     img = img.resize((largura, altura), Image.ANTIALIAS)
     return ImageTk.PhotoImage(img)
 
-# Função para criar um tooltip
 def criar_tooltip(widget, texto):
+    """Cria um tooltip para o widget fornecido."""
     tooltip = tk.Toplevel(root)
     tooltip.wm_overrideredirect(True)
     tooltip.wm_geometry(f"+{widget.winfo_pointerx()+10}+{widget.winfo_pointery()+10}")
     tooltip.configure(bg='white')
-    
+
     label = tk.Label(tooltip, text=texto, bg='white', fg='black', padx=5, pady=5, relief='solid')
     label.pack()
-    
+
     def mostrar_tooltip(event):
         tooltip.wm_geometry(f"+{event.x_root+10}+{event.y_root+10}")
         tooltip.deiconify()
-    
+
     def esconder_tooltip(event):
         tooltip.withdraw()
-    
+
     widget.bind("<Enter>", mostrar_tooltip)
     widget.bind("<Leave>", esconder_tooltip)
     tooltip.withdraw()
 
-# Função para alternar entre os frames
-def mostrar_frame():
-    main.destroy()
-    Frame_Process.grid(row=1, column= 0)
+def mostrar_tela_demanda():
+    root.destroy()
 
-# Configuração da interface Tkinter
+    # ---------- Tela Demanda ------------ #
+
+    root_Demanda = Tk()
+    root_Demanda.title("DEMANDA")
+    root_Demanda.iconbitmap("icon.ico")
+    root_Demanda.geometry('1000x600')
+    root_Demanda.resizable(False, False)
+    root_Demanda.configure(bg='#ccc')
+
+    # ------- Itens Necessários --------
+    # => Produtos e verificar quais estão ativos
+    # => e carregar seus respectivos fornecedores
+
+    
+
+
+
+
+
+    
+    root_Demanda.mainloop()
+
+
+def mostrar_tela_pedidos():
+    root.destroy()
+
+    # ---------- Tela Demanda ------------ #
+
+    root_Pedidos = Tk()
+    root_Pedidos.title("PEDIDOS")
+    root_Pedidos.iconbitmap("icon.ico")
+    root_Pedidos.geometry('1000x600')
+    root_Pedidos.resizable(False, False)
+    root_Pedidos.configure(bg='#ccc')
+    
+    root_Pedidos.mainloop()
+
+# ---------- Configuração da Interface Tkinter ---------- #
+
 root = Tk()
 root.title("DEMANDA AUTOMÁTICA")
 root.iconbitmap("icon.ico")
@@ -112,41 +144,45 @@ root.geometry('1000x600')
 root.resizable(False, False)
 root.configure(bg='#ccc')
 
-# ---- TELA HOME ---- #
-header =  tk.Frame(root, bg='#06074f', relief='solid', width=1000, height=80)
+# Header
+header = tk.Frame(root, bg='#06074f', relief='solid', width=1000, height=80)
 header.propagate(False)
-header.grid(row=0, column= 0)
+header.grid(row=0, column=0)
 
-# FRAME LOGO
+# Frame principal
 img_logo = redimensionar_imagem('logo.png', 200, 200)
 
-main =  tk.Frame(root,bg='#ccc', relief='solid', width=1000, height=720)
+main = tk.Frame(root, bg='#ccc', relief='solid', width=1000, height=720)
 main.propagate(False)
-main.grid(row=1, column= 0)
+main.grid(row=1, column=0)
 
-lbl_ImgLogo = Label(main, image= img_logo, width=200, height=200, bg="#ccc")
+lbl_ImgLogo = Label(main, image=img_logo, width=200, height=200, bg="#ccc")
 lbl_ImgLogo.pack(pady=120)
 
-# Frame Processar
-Frame_Process =  tk.Frame(root,bg='#ccc', relief='solid', width=1000, height=720)
+# Frame de Processamento
+Frame_Process = tk.Frame(root, bg='#ccc', relief='solid', width=1000, height=720)
 Frame_Process.propagate(False)
 
-#BTN EMPURRAR
+# Botão Empurrar
 img_empurrar = redimensionar_imagem('distribuicao.png', 70, 60)
 
-# --- BTN EMPURRAR --- #
-btnEmpurrar = tk.Button(header, image=img_empurrar, relief="solid", border=0, cursor='hand2', command=mostrar_frame)
-btnEmpurrar.place(x=860, y=8)
+btnEmpurrar = tk.Button(header, image=img_empurrar, relief="solid", border=0, cursor='hand2', command=mostrar_tela_demanda)
+btnEmpurrar.place(x=900, y=8)
+criar_tooltip(btnEmpurrar, "Empurrar Demanda")
 
-# Criar o tooltip para o botão
-criar_tooltip(btnEmpurrar, "Clique aqui para processar o arquivo Excel")
+# Botão Pedidos
+img_Comprar = redimensionar_imagem('shopping-cart.png', 70, 60)
 
+btnComprar = tk.Button(header, image=img_Comprar, relief="solid", border=0, cursor='hand2', command=mostrar_tela_pedidos)
+btnComprar.place(x=800, y=8)
+criar_tooltip(btnComprar, "Realizar Pedidos de Compra")
 
-img_upload = redimensionar_imagem('upload.png', 40 , 40)
+# Botão Carregar
+img_upload = redimensionar_imagem('upload.png', 40, 40)
 
-btn_carregar = Button(Frame_Process,image= img_upload ,text="CARREGAR",width=140,height=40,compound="left", relief="flat", cursor="hand2", command=carregar_e_processar_arquivo)
+btn_carregar = Button(Frame_Process, image=img_upload, text="CARREGAR", width=140, height=40, compound="left", relief="flat", cursor="hand2", command=carregar_e_processar_arquivo)
 btn_carregar.place(x=10, y=20)
-
 criar_tooltip(btn_carregar, "CARREGAR ARQUIVO DEMANDA")
 
+# Iniciar a interface
 root.mainloop()
